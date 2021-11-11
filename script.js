63fa3c74be92e5df8f30803e5a573756347d9f50
Made Wisnu <wisnukusuma99@gmail.com>
2021-11-11 17:42:51 +0800

    tester14

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-              latitude: '-8.603217',
-              longitude: '116.144195',
+              lat: '-8.603217',
+              long: '116.144195',
                 // decomment the following and add coordinates:
                 // lat: <your-latitude>,
                 // lng: <your-longitude>,
@@ -26,18 +26,18 @@ var models = [
         url: './assets/magnemite/scene.gltf',
         scale: '0.2 0.2 0.2',
         info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
+        //rotation: '0 180 0',
     },
     {
         url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
-        rotation: '0 180 0',
+        //rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: './assets/dragonite/scene.gltf',
         scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
+        //rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
 ];
